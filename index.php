664d46faf5957af1913b0f042dfd7f2cd412fda3
Simon Davies <simondavies@live.co.uk>
2015-10-25 21:34:00 +0000

    Init strucutre
    the first initial structure build for the demo site/example

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/src/start.php';
+
+use VeeziAPI\VeeziAPIWrapper as VeeziAPI;
+
+$Veezi = new VeeziAPI();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Veezi API Wrapper</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h2>Simple Veezi App</h2>
+
+    </div>
+</body>
+</html>
---
+++ src/VeeziAPI/VeeziAPIWrapper.php
@@ -0,0 +1,12 @@
+<?php
+namespace VeeziAPI;
+
+/**
+ *
+ */
+class VeeziAPIWrapper {
+
+	function __construct() {
+		die(var_dump('Veezi App has begun...'));
+	}
+}
